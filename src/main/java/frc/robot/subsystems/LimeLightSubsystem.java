95a6cc0c2011b1804a3320713c9713177a45dfca
JimmyCuffari <36243899+JimmyCuffari@users.noreply.github.com>
2020-02-17 13:17:57 -0500

    quick fix

--- src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -15,7 +15,7 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 //
 public class LimeLightSubsystem extends SubsystemBase {
-//
+
 NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 NetworkTableEntry tx = table.getEntry("tx");
 NetworkTableEntry ty = table.getEntry("ty");
